8720af8399c8a7a914889941736dc5388ca5840d
althjs <althjs@gmail.com>
2016-06-17 14:29:38 +0900

    for test (node 6)

--- test/require_helper.js
+++ test/require_helper.js
@@ -2,6 +2,6 @@
  * https://github.com/gregjopa/express-app-testing-demo
  */
 module.exports = function (path) {
-  var server = require('path').join(__dirname + '../../' + path);
+  var server = require('path').join(__dirname + './../' + path);
   return require(server);
 };
